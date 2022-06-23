eeef26213d5faf425c14ffadc7332fde143843e3
Oscar Lopez <oscareduardolp6@gmail.com>
2022-06-22 22:25:24 -0500

    Added initialized object

--- ProductDTO.ts
+++ ProductDTO.ts
@@ -15,3 +15,21 @@ export interface ProductDTO {
   showSiteQuantity: number
   privateSiteQuantity: number
 }
+
+export const initializedProductDTO: ProductDTO = {
+  id                    : '', 
+  name                  : '', 
+  description           : '', 
+  brand                 : '', 
+  color                 : '', 
+  fabric                : '', 
+  size                  : '', 
+  suppliers             : '',
+  price                 : 0, 
+  mortgagePrice         : 0, 
+  privateSiteQuantity   : 0, 
+  publicPrice           : 0, 
+  showSiteQuantity      : 0, 
+  alertLowStockQuantity : 0, 
+  shouldAlertLowStock   : false
+}
