96672bcf13ba0248695f07db7e9578bf7507719f
Oscar Lopez <oscareduardolp6@gmail.com>
2022-07-01 22:22:24 -0500

    Agregado el tipo de transacción desde el Enum

--- OrderRowDTO.ts
+++ OrderRowDTO.ts
@@ -1,8 +1,10 @@
+import { TransactionTypes } from "./TransactionDTO.ts";
+
 export interface OrderRowDTO {
   productID: string 
   clientID: string
   privateSiteQuantity: number
   publicSiteQuantity: number
   sellerID: string
-  sellType: string 
+  sellType: TransactionTypes
 }
