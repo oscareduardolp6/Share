116ee8dff3bdc000336f8e61430fc13633c64b6b
Oscar Lopez <oscareduardolp6@gmail.com>
2022-07-01 22:20:08 -0500

    Agregado el tipo de venta

--- TransactionDTO.ts
+++ TransactionDTO.ts
@@ -16,17 +16,19 @@ export interface TransactionDTO {
 }
 
 export enum TransactionTypes {
-  Sale = 1,
+  PublicSale = 1,
   Purchase = 2, 
   Error = 3, 
-  SiteMovement = 4
+  SiteMovement = 4, 
+  MortgageSale = 5
 }
 
 const descriptionsTransactionsTypes = {
-  1: 'Venta', 
+  1: 'Venta Pública', 
   2: 'Compra', 
   3: 'Error', 
-  4: 'Movimiento entre almacenes'
+  4: 'Movimiento entre almacenes', 
+  5: 'Venta Hipóteca'
 }
 
 export const transactionTypesDescription = Object.freeze(descriptionsTransactionsTypes)
